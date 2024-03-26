bdc9161577d433287aaad722a31f9dccae499d95
nakayama <itsukiglucose@gmail.com>
2024-03-27 04:13:16 +0900

    delele test post

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20240327040347';
+const version = '20240327041122';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2024/03/27/test/","/general/2020/01/01/test/","/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2020/01/01/test/","/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
